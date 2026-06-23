eeb20254347ab813b701a464545e5bfd500fd6aa
clackagenciaof-ship-it <clackagenciaof@gmail.com>
2026-06-23 06:19:44 -0300

    Mostra painel de funil nas configurações

--- src/components/settings/SettingsPageWithAdmin.tsx
+++ src/components/settings/SettingsPageWithAdmin.tsx
@@ -2,6 +2,7 @@
 
 import { AuditPanel } from './AuditPanel';
 import { CompanyAdminPanel } from './CompanyAdminPanel';
+import { FunnelAdvancedPanel } from './FunnelAdvancedPanel';
 import { SettingsPage as BaseSettingsPage } from './SettingsPage';
 import type { UserRole } from '@/types/crm';
 
@@ -17,6 +18,9 @@ export function SettingsPage(props: SettingsPageProps) {
       <div className="grid two-col" style={{ marginBottom: 16 }}>
         <CompanyAdminPanel />
         <AuditPanel />
+      </div>
+      <div style={{ marginBottom: 16 }}>
+        <FunnelAdvancedPanel />
       </div>
       <BaseSettingsPage {...props} />
     </>
